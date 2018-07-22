7451e76f7acd3807be889a2d2123c49bf5a29605
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-07-22 19:24:38 +0530

    Commit for Property Source

---
+++ src/test/java/com/myprojects/spring/examples/springdevopsaws/test/external/props/PropertySourceTest.java
@@ -0,0 +1,28 @@
+package com.myprojects.spring.examples.springdevopsaws.test.external.props;
+
+
+import com.myprojects.spring.examples.springdevopsaws.test.jms.FakeJmsBroker;
+import com.myprojects.spring.examples.test.config.external.props.ExternalPropsPropertySourceTestConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ExternalPropsPropertySourceTestConfig.class)
+public class PropertySourceTest {
+
+    @Autowired
+    FakeJmsBroker fakeJmsBroker;
+
+    @Test
+    public void testPropsSet() throws Exception {
+        assertEquals("10.10.10.123", fakeJmsBroker.getUrl());
+        assertEquals(3330, fakeJmsBroker.getPort().intValue());
+        assertEquals("Ron", fakeJmsBroker.getUser());
+        assertEquals("Burgundy", fakeJmsBroker.getPassword());
+    }
+}
